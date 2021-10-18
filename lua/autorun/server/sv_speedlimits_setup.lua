92ca5c88f2a7583cb61b6885001ecc3f9df92514
Redox <69946827+wrefgtzweve@users.noreply.github.com>
2021-10-18 19:47:27 +0200

    Limit angular velocity but leave velocity for now

--- lua/autorun/server/sv_speedlimits_setup.lua
+++ lua/autorun/server/sv_speedlimits_setup.lua
@@ -1,11 +1,12 @@
-local magicNumber = 11784960000
+local maxVelocity = 11784960000 -- Default is exactly: 4000
+local maxAngularVelocity = 100000 -- Default is exactly: 7272.7280273438
 
 hook.Add( "InitPostEntity", "CFC_SpeedLimits_Setup", function()
     ORIGINAL_PERFORMANCE_SETTINGS = physenv.GetPerformanceSettings()
 
     local tbl = table.Copy( ORIGINAL_PERFORMANCE_SETTINGS )
-    tbl.MaxAngularVelocity = magicNumber
-    tbl.MaxVelocity = magicNumber
+    tbl.MaxAngularVelocity = maxAngularVelocity
+    tbl.MaxVelocity = maxVelocity
 
     physenv.SetPerformanceSettings( tbl )
 end )
